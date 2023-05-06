cb9f2290987afcf99c047262267170e9ffc6958c
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-05-06 11:38:11 -0300

    :sparkles: feat: API Routes de Avaliações Recentes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
 
 import type { AppProps } from 'next/app'
+import { queryClient } from '@/lib/react-query'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import { Nunito } from 'next/font/google'
 import { globalStyles } from '@/styles/global'
@@ -26,10 +28,12 @@ export default function App({
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
-    <SessionProvider session={session}>
-      <div className={`${nunito.className}`}>
-        {getLayout(<Component {...pageProps} />)}
-      </div>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <div className={`${nunito.className}`}>
+          {getLayout(<Component {...pageProps} />)}
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
